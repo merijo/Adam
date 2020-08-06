371463aaffd244f646972ff04001c521dcc1031e
MERIN JOSEPH <56555523+merijo@users.noreply.github.com>
2020-08-06 14:05:02 -0400

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HouseHoldComponent } from './house-hold/house-hold.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HouseHoldComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
